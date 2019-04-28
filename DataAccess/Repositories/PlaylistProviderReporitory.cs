2e50c064ed43e107d6e2cf9da44d303dbc5fea9f
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-04-28 17:53:07 +0300

    Handle commented CSV lines

--- DataAccess/Repositories/PlaylistProviderReporitory.cs
+++ DataAccess/Repositories/PlaylistProviderReporitory.cs
@@ -10,6 +10,7 @@ namespace IptvPlaylistFetcher.DataAccess.Repositories
     public sealed class PlaylistProviderRepository : IPlaylistProviderRepository
     {
         const char CsvFieldSeparator = ',';
+        const char CommentCharacter = '#';
 
         readonly ApplicationSettings settings;
 
@@ -31,7 +32,11 @@ namespace IptvPlaylistFetcher.DataAccess.Repositories
                 }
     
                 PlaylistProviderEntity entity = ReadEntity(line);
-                entities.Add(entity);
+
+                if (!(entity is null))
+                {
+                    entities.Add(entity);
+                }
             }
 
             return entities;
@@ -39,6 +44,17 @@ namespace IptvPlaylistFetcher.DataAccess.Repositories
 
         public static PlaylistProviderEntity ReadEntity(string csvLine)
         {
+            if (!(csvLine is null) &&
+                csvLine.Contains(CommentCharacter))
+            {
+                csvLine = csvLine.Substring(0, csvLine.IndexOf(CommentCharacter));
+            }
+
+            if (string.IsNullOrWhiteSpace(csvLine))
+            {
+                return null;
+            }
+
             string[] fields = csvLine.Split(CsvFieldSeparator);
 
             PlaylistProviderEntity entity = new PlaylistProviderEntity();
