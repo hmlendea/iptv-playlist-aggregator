a51e7f5e8f0e759b451478ff82f19e7cc1b10915
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-02-03 22:10:55 +0200

    Fixed channel matching

--- UnitTests/Service/ChannelMatcherTests.cs
+++ UnitTests/Service/ChannelMatcherTests.cs
@@ -10,7 +10,7 @@ namespace IptvPlaylistAggregator.UnitTests.Service.Models
     public sealed class ChannelMatcherTests
     {
         Mock<ICacheManager> cacheMock;
-        
+
         IChannelMatcher channelMatcher;
 
         [SetUp]
@@ -21,50 +21,27 @@ namespace IptvPlaylistAggregator.UnitTests.Service.Models
             channelMatcher = new ChannelMatcher(cacheMock.Object);
         }
 
+        [TestCase("Ardeal TV", "RO: Ardeal TV", "|RO| Ardeal TV")]
+        [TestCase("Cartoon Network", "RO: Cartoon Network", "VIP|RO|: Cartoon Network")]
+        [TestCase("Digi Sport 2", "RO: Digi Sport 2", "RO: DIGI Sport 2")]
+        [TestCase("România TV", "România TV", "RO\" Romania TV")]
+        [TestCase("Somax TV", null, "Somax TV")]
         [Test]
-        public void DoesMatch_CompareWithSameValue_ReturnsTrue()
+        public void DoesMatch_NamesMatch_ReturnsTrue(
+            string definedName,
+            string alias,
+            string providerName)
         {
-            string definedName = "Digi Sport 2";
-            string providerName = "RO: Digi Sport 2";
-            string alias = "RO: Digi Sport 2";
-
-            ChannelName channelName = new ChannelName(definedName, alias);
-
-            Assert.IsTrue(channelMatcher.DoesMatch(channelName, providerName));
-        }
-
-        [Test]
-        public void DoesMatch_CompareWithSameValueDifferentCasing_ReturnsTrue()
-        {
-            string definedName = "Digi Sport 2";
-            string providerName = "RO: Digi Sport 2";
-            string alias = "RO: DIGI Sport 2";
-
-            ChannelName channelName = new ChannelName(definedName, alias);
-
-            Assert.IsTrue(channelMatcher.DoesMatch(channelName, providerName));
-        }
-
-        [Test]
-        public void DoesMatch_CompareWithSameValueWithDiacritics_ReturnsTrue()
-        {
-            string definedName = "TVR Timișoara";
-            string providerName = "RO: TVR Timisoara";
-            string alias = "RO: TVR Timișoara";
-
-            ChannelName channelName = new ChannelName(definedName, alias);
-
-            Assert.IsTrue(channelMatcher.DoesMatch(channelName, providerName));
-        }
-
-        [Test]
-        public void DoesMatch_CompareWithSameValueWithBlacklistedSubstrings_ReturnsTrue()
-        {
-            string definedName = "Digi Sport 2";
-            string providerName = "RO: Digi Sport 2 [Multi-Audio]";
-            string alias = "RO: Digi Sport 2";
-
-            ChannelName channelName = new ChannelName(definedName, alias);
+            ChannelName channelName;
+            
+            if (alias is null)
+            {
+                channelName = new ChannelName(definedName);
+            }
+            else
+            {
+                channelName = new ChannelName(definedName, alias);
+            }
 
             Assert.IsTrue(channelMatcher.DoesMatch(channelName, providerName));
         }
@@ -72,13 +49,22 @@ namespace IptvPlaylistAggregator.UnitTests.Service.Models
         [Test]
         public void DoesMatch_CompareWithDifferentValue_ReturnsFalse()
         {
-            string definedName = "Digi Sport 2";
+            string definedName = "Telekom Sport 2";
             string providerName = "RO: Digi Sport 2";
             string alias = "RO: Telekom Sport 2";
 
             ChannelName channelName = new ChannelName(definedName, alias);
 
             Assert.IsFalse(channelMatcher.DoesMatch(channelName, providerName));
+        }
+
+        [TestCase("VIP|RO|: Discovery Channel FHD", "DISCOVERYCHANNEL")]
+        [Test]
+        public void NormaliseName_ReturnsExpectedValue(string inputValue, string expectedValue)
+        {
+            string actualValue = channelMatcher.NormaliseName(inputValue);
+            
+            Assert.AreEqual(expectedValue, actualValue);
         }
     }
 }
