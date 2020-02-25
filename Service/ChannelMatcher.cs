167f3116e8cae8f0766392f4a2c0eb2d0651d0a6
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-02-25 15:26:32 +0200

    Added more channel matching rules

--- Service/ChannelMatcher.cs
+++ Service/ChannelMatcher.cs
@@ -29,8 +29,8 @@ namespace IptvPlaylistAggregator.Service
 
             { " HEVC$", "" },
             { " HEVC ", "" },
-            { " [FU]*[HMS][DQ]$", "" },
-            { " [FU]*[HMS][DQ] ", "" },
+            { " (Ultra|[FU])*_*[HMS][DQ]$", "" },
+            { " (Ultra|[FU])*_*[HMS][DQ] ", "" },
             { "\\(Opt-[0-9]\\)", "" },
             { "\\[[0-9]*p\\]", "" },
             { " S[0-9]$", "" },
