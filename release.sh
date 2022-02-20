0841b961b219c9eac22e72a6d074198f57df953c
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-02-20 12:16:07 +0200

    Upgraded to .NET 6

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DOTNET_VERSION="5.0"
+DOTNET_VERSION="6.0"
 RELEASE_SCRIPT_URL="https://raw.githubusercontent.com/hmlendea/deployment-scripts/master/release/dotnet/${DOTNET_VERSION}.sh"
 wget --quiet -O - "${RELEASE_SCRIPT_URL}" | bash /dev/stdin ${@}
