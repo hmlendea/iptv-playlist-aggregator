22a60fcca7db90d9a8912440dff3d5de55af67e1
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-06-16 10:19:47 +0300

    Disabled release trimming

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOTNET_VERSION="7.0"
 RELEASE_SCRIPT_URL="https://raw.githubusercontent.com/hmlendea/deployment-scripts/master/release/dotnet/${DOTNET_VERSION}.sh"
-wget --quiet -O - "${RELEASE_SCRIPT_URL}" | bash /dev/stdin ${@}
+curl -sSL "${RELEASE_SCRIPT_URL}" | bash -s ${1} --no-trim
